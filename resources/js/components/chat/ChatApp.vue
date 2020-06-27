7d767d98be144ea8955cd82c623d33fd650ddf82
mahmoud-ow <a7ascape@gmail.com>
2020-06-27 09:41:40 +0200

    add chat vue files

---
+++ resources/js/components/chat/ChatApp.vue
@@ -0,0 +1,72 @@
+<template>
+    <div class="chat-app">
+        <Conversation
+            :contact="selectedContact"
+            :messages="messages"
+            @new="saveNewMessage"
+        />
+        <ContactsList :contacts="contacts" @selected="startConversationWith" />
+    </div>
+</template>
+
+<script>
+import Conversation from "./Conversation";
+import ContactsList from "./ContactsList";
+
+export default {
+    props: {
+        user: {
+            type: Object,
+            required: true
+        }
+    },
+    data() {
+        return {
+            selectedContact: null,
+            messages: [],
+            contacts: []
+        };
+    },
+    mounted() {
+        //console.log(this.user);
+
+        Echo.private(`messages.${this.user.id}`).listen("NewMessage", e => {
+            this.handleIncoming(e.message);
+        });
+
+        axios.get("/contacts").then(response => {
+            this.contacts = response.data;
+        });
+    },
+    methods: {
+        startConversationWith(contact) {
+            axios.get(`/conversations/${contact.id}`).then(response => {
+                this.messages = response.data;
+                this.selectedContact = contact;
+            });
+        },
+        saveNewMessage(text) {
+            this.messages.push(text);
+        },
+
+        handleIncoming(message) {
+            if (
+                this.selectedContact &&
+                message.from == this.selectedContact.id
+            ) {
+                this.saveNewMessage(message);
+                return;
+            }
+
+            alert(message.text);
+        }
+    },
+    components: { Conversation, ContactsList }
+};
+</script>
+
+<style lang="scss" scoped>
+.chat-app {
+    display: flex;
+}
+</style>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-sm-12">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">We Code Messenger</div>
 
 
 
