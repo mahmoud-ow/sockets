2b5100be0cc3ef5414a32c0c72b26c54ade33162
mahmoud-ow <a7ascape@gmail.com>
2020-06-27 02:24:42 +0200

    add npm 3

--- config/websockets.php
+++ config/websockets.php
@@ -110,18 +110,19 @@ return [
          * certificate chain of issuers. The private key also may be contained
          * in a separate file specified by local_pk.
          */
-        'local_cert' => env('LOCAL_CERT', null),
+        'local_cert' => null,
 
         /*
          * Path to local private key file on filesystem in case of separate files for
          * certificate (local_cert) and private key.
          */
-        'local_pk' => env('LOCAL_PK', null),
+        'local_pk' => null,
 
         /*
          * Passphrase for your local_cert file.
          */
         'passphrase' => null,
+        'verify_peer' => false,
     ],
 
     /*
