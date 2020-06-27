7a6821f343a0333bda6f255555863eb840aa9146
mahmoud-ow <a7ascape@gmail.com>
2020-06-27 10:53:04 +0200

    chat arrange tabs if open

--- public/js/app.js
+++ public/js/app.js
@@ -2008,6 +2008,8 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
+//
+//
 /* harmony default export */ __webpack_exports__["default"] = ({
   props: {
     contacts: {
@@ -2017,13 +2019,26 @@ __webpack_require__.r(__webpack_exports__);
   },
   data: function data() {
     return {
-      selected: 0
+      selected: this.contacts.length ? this.contacts[0] : null
     };
   },
   methods: {
-    selectContact: function selectContact(index, contact) {
-      this.selected = index;
+    selectContact: function selectContact(contact) {
+      this.selected = contact;
       this.$emit("selected", contact);
+    }
+  },
+  computed: {
+    sortedContacts: function sortedContacts() {
+      var _this = this;
+
+      return _.sortBy(this.contacts, [function (contact) {
+        if (contact == _this.selected) {
+          return Infinity;
+        }
+
+        return contact.unread;
+      }]).reverse();
     }
   }
 });
@@ -6646,7 +6661,7 @@ exports = module.exports = __webpack_require__(/*! ../../../../node_modules/css-
 
 
 // module
-exports.push([module.i, ".contacts-list[data-v-7d33fe4d] {\n  flex: 2;\n  max-height: 600px;\n  overflow: scroll;\n  border-left: 1px solid #ddd;\n}\n.contacts-list ul[data-v-7d33fe4d] {\n  list-style-type: none;\n  padding-left: 0;\n}\n.contacts-list ul li[data-v-7d33fe4d] {\n  display: flex;\n  padding: 2px;\n  border-bottom: 1px solid #DDD;\n  height: 80px;\n  position: relative;\n  cursor: pointer;\n}\n.contacts-list ul li.selected[data-v-7d33fe4d] {\n  background-color: #dfdfdf;\n}\n.contacts-list ul li span.unread[data-v-7d33fe4d] {\n  background-color: #82e0e8;\n  position: absolute;\n  right: 11px;\n  top: 20px;\n  display: flex;\n  font-weight: 700;\n  min-width: 20px;\n  justify-content: center;\n  align-items: center;\n  line-height: 20px;\n  font-size: 12px;\n  padding: 0 4px;\n  border-radius: 3px;\n}\n.contacts-list ul .avatar[data-v-7d33fe4d] {\n  flex: 1;\n  display: flex;\n  align-items: center;\n}\n.contacts-list ul .avatar img[data-v-7d33fe4d] {\n  width: 35px;\n  border-radius: 50px;\n  margin: 0 auto;\n}\n.contacts-list ul .contact[data-v-7d33fe4d] {\n  flex: 3;\n  font-size: 10px;\n  overflow: hidden;\n  display: flex;\n  flex-direction: column;\n  justify-content: center;\n}\n.contacts-list ul .contact p[data-v-7d33fe4d] {\n  margin: 0;\n}\n.contacts-list ul .contact p.name[data-v-7d33fe4d] {\n  font-weight: bold;\n}", ""]);
+exports.push([module.i, ".contacts-list[data-v-7d33fe4d] {\n  flex: 2;\n  max-height: 600px;\n  overflow: scroll;\n  border-left: 1px solid #ddd;\n}\n.contacts-list ul[data-v-7d33fe4d] {\n  list-style-type: none;\n  padding-left: 0;\n}\n.contacts-list ul li[data-v-7d33fe4d] {\n  display: flex;\n  padding: 2px;\n  border-bottom: 1px solid #ddd;\n  height: 80px;\n  position: relative;\n  cursor: pointer;\n}\n.contacts-list ul li.selected[data-v-7d33fe4d] {\n  background-color: #dfdfdf;\n}\n.contacts-list ul li span.unread[data-v-7d33fe4d] {\n  background-color: #82e0e8;\n  position: absolute;\n  right: 11px;\n  top: 20px;\n  display: flex;\n  font-weight: 700;\n  min-width: 20px;\n  justify-content: center;\n  align-items: center;\n  line-height: 20px;\n  font-size: 12px;\n  padding: 0 4px;\n  border-radius: 3px;\n}\n.contacts-list ul .avatar[data-v-7d33fe4d] {\n  flex: 1;\n  display: flex;\n  align-items: center;\n}\n.contacts-list ul .avatar img[data-v-7d33fe4d] {\n  width: 35px;\n  border-radius: 50px;\n  margin: 0 auto;\n}\n.contacts-list ul .contact[data-v-7d33fe4d] {\n  flex: 3;\n  font-size: 10px;\n  overflow: hidden;\n  display: flex;\n  flex-direction: column;\n  justify-content: center;\n}\n.contacts-list ul .contact p[data-v-7d33fe4d] {\n  margin: 0;\n}\n.contacts-list ul .contact p.name[data-v-7d33fe4d] {\n  font-weight: bold;\n}", ""]);
 
 // exports
 
@@ -44635,15 +44650,15 @@ var render = function() {
   return _c("div", { staticClass: "contacts-list" }, [
     _c(
       "ul",
-      _vm._l(_vm.contacts, function(contact, index) {
+      _vm._l(_vm.sortedContacts, function(contact) {
         return _c(
           "li",
           {
             key: contact.id,
-            class: { selected: index == _vm.selected },
+            class: { selected: contact == _vm.selected },
             on: {
               click: function($event) {
-                return _vm.selectContact(index, contact)
+                return _vm.selectContact(contact)
               }
             }
           },
@@ -44662,7 +44677,7 @@ var render = function() {
             _vm._v(" "),
             contact.unread
               ? _c("span", { staticClass: "unread" }, [
-                  _vm._v(_vm._s(contact.unread) + " ")
+                  _vm._v(_vm._s(contact.unread) + "\n            ")
                 ])
               : _vm._e()
           ]
--- resources/js/components/chat/ContactsList.vue
+++ resources/js/components/chat/ContactsList.vue
@@ -2,10 +2,10 @@
     <div class="contacts-list">
         <ul>
             <li
-                v-for="(contact, index) in contacts"
+                v-for="contact in sortedContacts"
                 :key="contact.id"
-                @click="selectContact(index, contact)"
-                :class="{ selected: index == selected }"
+                @click="selectContact(contact)"
+                :class="{ selected: contact == selected }"
             >
                 <div class="avatar">
                     <img :src="contact.profile_image" :alt="contact.name" />
@@ -14,7 +14,9 @@
                     <p class="name">{{ contact.name }}</p>
                     <p class="email">{{ contact.email }}</p>
                 </div>
-                <span class="unread" v-if="contact.unread">{{ contact.unread }} </span>
+                <span class="unread" v-if="contact.unread"
+                    >{{ contact.unread }}
+                </span>
             </li>
         </ul>
     </div>
@@ -31,14 +33,26 @@ export default {
 
     data() {
         return {
-            selected: 0
+            selected: this.contacts.length ? this.contacts[0] : null
         };
     },
 
     methods: {
-        selectContact(index, contact) {
-            this.selected = index;
+        selectContact(contact) {
+            this.selected = contact;
             this.$emit("selected", contact);
+        }
+    },
+    computed: {
+        sortedContacts() {
+            return _.sortBy(this.contacts, [
+                contact => {
+                    if (contact == this.selected) {
+                        return Infinity;
+                    }
+                    return contact.unread;
+                }
+            ]).reverse();
         }
     }
 };
@@ -55,64 +69,62 @@ export default {
         list-style-type: none;
         padding-left: 0;
 
-        li{
-          display: flex;
-          padding: 2px;
-          border-bottom: 1px solid #DDD;
-          height: 80px;
-          position: relative;
-          cursor: pointer;
+        li {
+            display: flex;
+            padding: 2px;
+            border-bottom: 1px solid #ddd;
+            height: 80px;
+            position: relative;
+            cursor: pointer;
 
-
-          &.selected{
-            background-color: #dfdfdf;
-          }
-
-          span.unread{
-              background-color: #82e0e8;
-              position: absolute;
-              right: 11px;
-              top: 20px;
-              display: flex;
-              font-weight: 700;
-              min-width: 20px;
-              justify-content: center;
-              align-items: center;
-              line-height: 20px;
-              font-size: 12px;
-              padding: 0 4px;
-              border-radius: 3px;
-          }
-
-        }
-
-        .avatar{
-          flex: 1;
-          display: flex;
-          align-items: center;
-
-          img{
-            width: 35px;
-            border-radius: 50px;
-            margin: 0 auto;
-          }
-        }
-
-        .contact{
-          flex: 3;
-          font-size: 10px;
-          overflow: hidden;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-
-          p{
-            margin: 0;
-
-            &.name{
-              font-weight: bold;
+            &.selected {
+                background-color: #dfdfdf;
             }
-          }
+
+            span.unread {
+                background-color: #82e0e8;
+                position: absolute;
+                right: 11px;
+                top: 20px;
+                display: flex;
+                font-weight: 700;
+                min-width: 20px;
+                justify-content: center;
+                align-items: center;
+                line-height: 20px;
+                font-size: 12px;
+                padding: 0 4px;
+                border-radius: 3px;
+            }
+        }
+
+        .avatar {
+            flex: 1;
+            display: flex;
+            align-items: center;
+
+            img {
+                width: 35px;
+                border-radius: 50px;
+                margin: 0 auto;
+            }
+        }
+
+        .contact {
+            flex: 3;
+            font-size: 10px;
+            overflow: hidden;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+
+            p {
+                margin: 0;
+
+                &.name {
+                    font-weight: bold;
+                }
+            }
         }
     }
 }
