3f17dbef86a3b8f4288b282f96b3a77edd1dbb23
mahmoud-ow <a7ascape@gmail.com>
2020-06-27 07:58:03 +0200

    sep files

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -68,7 +68,7 @@
 </head>
 
 <body>
-    <div class="flex-center position-ref full-height">
+    <div id="app" class="flex-center position-ref full-height">
         @if (Route::has('login'))
         <div class="top-right links">
             @auth
