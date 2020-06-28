11a8bd44e08c0a07bb9995591b6f0fcc775917d6
mahmoud-ow <a7ascape@gmail.com>
2020-06-28 07:47:29 +0200

    mysql 5

--- app/Http/Controllers/ContactsController.php
+++ app/Http/Controllers/ContactsController.php
@@ -16,9 +16,13 @@ class ContactsController extends Controller
         
         $from_ids = Message::selectRaw(' `to` , `from` ')->where( 'to' , auth()->id() )->groupBy('to', 'from')->get();
         $to_ids = Message::selectRaw(' `to` , `from` ')->where( 'from' , auth()->id() )->groupBy('to', 'from')->get();
-        //Message::selectRaw(' ANY_VALUE(`from`), `to`')->where( 'to' , auth()->id() )->groupBy('to')->get();
         
+        $valid_users = [];
+        foreach( $from_ids as $from_user ){
+            $valid_users[] = $from_user->from;
+        }
         
+        return $valid_users;
         return $from_ids .'<hr />' . $to_ids;
 
         // get all users except the auth()
