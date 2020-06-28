81367408292db520fb9184a0c76964c4814a57a8
mahmoud-ow <a7ascape@gmail.com>
2020-06-28 21:13:15 +0200

    chat fix 21

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -112,10 +112,12 @@
     <script>
         $(document).ready(function(){
 
-            /* Echo.channel('home')
+            /* 
+            Echo.channel('home')
             .listen('NewMessage', (e) => {
                 console.log(e.message);
-            }); */
+            }); 
+            */
             
         });/* /ready() */
 
