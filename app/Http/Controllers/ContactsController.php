9c910099f28bb10ba429283e32e21de4fa7f2c41
mahmoud-ow <a7ascape@gmail.com>
2020-06-29 07:23:02 +0200

    update contact list 28

--- app/Http/Controllers/ContactsController.php
+++ app/Http/Controllers/ContactsController.php
@@ -46,7 +46,7 @@ class ContactsController extends Controller
         }
 
 
-        return array_unique( $valid_users );
+        return array_reverse(array_unique(array_reverse($valid_users)));;
         return $newArray;
 
         $contact_list = [];
