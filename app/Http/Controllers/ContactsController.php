3c4062d643d1f094d1e81439ebdfc180d600aa22
mahmoud-ow <a7ascape@gmail.com>
2020-06-29 02:59:34 +0200

    chat rearrange contacts 6

--- app/Http/Controllers/ContactsController.php
+++ app/Http/Controllers/ContactsController.php
@@ -22,7 +22,7 @@ class ContactsController extends Controller
             $valid_users[$from_user->rowid] = $from_user->from;
         }
         foreach( $to_ids as $to_user ){
-            $valid_users[$from_user->rowid] = $to_user->to;
+            $valid_users[$to_ids->rowid] = $to_user->to;
         }
         //$valid_users = array_unique($valid_users);
 
